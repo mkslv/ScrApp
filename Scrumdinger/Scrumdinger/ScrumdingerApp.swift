62523a0bcd2c2688422883f77edf0a391bf43841
Max Kiselyov <maxkiselyoff@gmail.com>
2023-02-04 23:23:41 +0000

    initial commit

---
+++ Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Max Kiselyov on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
