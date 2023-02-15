4b46b587913dc87986b4aa914739663632f1b364
Max Kiselyov <maxkiselyoff@gmail.com>
2023-02-15 12:55:19 +0000

    Detail view created Added capitalize name of the color in model

--- Scrumdinger/Scrumdinger/DetailView.swift
+++ Scrumdinger/Scrumdinger/DetailView.swift
@@ -11,7 +11,31 @@ struct DetailView: View {
     let scrum: DailyScrum
     
     var body: some View {
-        Text("Hello, kurwa!")
+        List{
+            Section(header: Text("Meeting info")) {
+                Label("Start meeting", systemImage: "timer") //  По молчанию VoiceOver читает тайтл этого лэйбла. Если ты хочешь чтобы VoiceOver читал больше деталей, замени дефолтный текст с помощью добавления accesibilityLabel
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text("\(scrum.theme.name)")
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(6)
+                }
+                .accessibilityElement(children: .combine)
+                
+                
+            }
+        }
     }
 }
 
